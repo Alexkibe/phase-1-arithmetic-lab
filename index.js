eea145516d7f7728e900044a9a9e512c0317b894
Alex Kibe <alex.kibe01@student.moringaschool.com>
2023-03-20 12:07:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+function getRandomint(){
+    Math.random()
+}
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 29
+const num4 = 5
+const mod = num3 % num4
+
+ let max = Math.max(20);
+ console.log(max);
+ 
+
+
